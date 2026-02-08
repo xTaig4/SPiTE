d2f7c5bb3ab63419c99e250f5ee489ca77a51454
Taig4 <xTaig4@gmail.com>
2026-02-08 13:29:44 +0100

    centered header title, added placeholder icon at bottom

--- spite/app/(tabs)/_layout.tsx
+++ spite/app/(tabs)/_layout.tsx
@@ -2,6 +2,7 @@ import { Tabs } from "expo-router";
 import React from "react";
 import { StyleSheet } from "react-native";
 import { useTheme } from "@/hooks/use-theme";
+import { Image } from "expo-image";
 
 export default function TabLayout() {
   const { theme } = useTheme();
@@ -11,9 +12,19 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: "Home",
+          title: "SPiTE",
+          headerTitleAlign: "center",
+          tabBarIcon: ({ color, focused }) => (
+            <Image
+              source={{
+                uri: "https://i.pinimg.com/1200x/14/ea/2a/14ea2a59a2ec3cd1a50ef4c7e26274c1.jpg",
+              }}
+              style={{ width: 24, height: 24, borderRadius: 10 }}
+            />
+          ),
           tabBarStyle: {
             backgroundColor: theme.background,
+            justifyContent: "center",
           },
         }}
       />
