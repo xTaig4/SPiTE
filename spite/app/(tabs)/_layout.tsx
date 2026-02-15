84e842b868013bc83cace304287d1c53ad0d0962
Taig4 <xTaig4@gmail.com>
2026-02-15 18:34:30 +0100

    onPress set to call handle authorization

--- spite/app/(tabs)/_layout.tsx
+++ spite/app/(tabs)/_layout.tsx
@@ -1,11 +1,20 @@
 import { Tabs } from "expo-router";
 import React from "react";
-import { StyleSheet } from "react-native";
+import { Pressable, StyleSheet } from "react-native";
 import { useTheme } from "@/hooks/use-theme";
 import { Image } from "expo-image";
+import { spotifyAuth } from "../services/auth/spotifyAuth";
 
 export default function TabLayout() {
   const { theme } = useTheme();
+
+  const handleSpotifyAuth = async () => {
+    const tokens = await spotifyAuth();
+    if (tokens) {
+      console.log("Access Token:", tokens.accessToken);
+      console.log("Refresh Token:", tokens.refreshToken);
+    }
+  };
 
   return (
     <Tabs>
@@ -15,12 +24,14 @@ export default function TabLayout() {
           title: "SPiTE",
           headerTitleAlign: "center",
           tabBarIcon: ({ color, focused }) => (
-            <Image
-              source={{
-                uri: "https://i.pinimg.com/1200x/14/ea/2a/14ea2a59a2ec3cd1a50ef4c7e26274c1.jpg",
-              }}
-              style={{ width: 24, height: 24, borderRadius: 10 }}
-            />
+            <Pressable onPress={handleSpotifyAuth}>
+              <Image
+                source={{
+                  uri: "https://i.pinimg.com/1200x/14/ea/2a/14ea2a59a2ec3cd1a50ef4c7e26274c1.jpg",
+                }}
+                style={{ width: 24, height: 24, borderRadius: 10 }}
+              />
+            </Pressable>
           ),
           tabBarStyle: {
             backgroundColor: theme.background,
